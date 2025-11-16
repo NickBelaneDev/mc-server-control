011a86fae8346c950abed4ab35ba12eb657e56e7
NickBelaneDev <ro.millotat@t-online.de>
2025-11-16 23:58:35 +0100

    First Commit

--- first_try.py
+++ first_try.py
@@ -4,7 +4,7 @@ print("--- Python-Skript startet ---")
 
 # Wir definieren den Befehl, den wir ausführen wollen.
 # WICHTIG: Wir übergeben ihn als Liste von Strings.
-befehl = ["ls", "-l"]
+befehl = ["ls"]
 
 subprocess.run(befehl)
 print("--- Python-Skript beendet ---\n")
