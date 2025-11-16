35cf85e2f7fbdbea24e490b223279d239141cad9
NickBelaneDev <ro.millotat@t-online.de>
2025-11-17 00:16:38 +0100

    First Commit

--- first_try.py
+++ first_try.py
@@ -5,16 +5,24 @@ print("--- Python-Skript startet ---\n")
 # Wir definieren den Befehl, den wir ausführen wollen.
 # WICHTIG: Wir übergeben ihn als Liste von Strings.
 
-def check_pwd():
-    befehl = ["pwd"]
+def _run(command: list[str], target: str):
+    befehl = command
+    ziel_ordner = target
+    # Führe den Befehl aus UND fange die Ausgabe in der 'result'-Variable
+    result = subprocess.run(befehl,
+                            capture_output=True,
+                            text=True,
+                            cwd=ziel_ordner)
 
-    _result = subprocess.run(
-        befehl,
-        capture_output=True,
-        text=True)
-    return _result
+    # Jetzt gehört die Ausgabe uns!
+    print("--- Python hat die Ausgabe gefangen ---")
+    print("Das ist der Standard-Output (stdout):")
+    print(result.stdout)
 
-result = check_pwd()
-print(f"{result.stderr=}")
-print(f"{result.stdout=}")
-print("\n--- Python-Skript beendet ---")
+    # Wir können auch nach Fehlern schauen (stderr)
+    print("Das ist der Standard-Fehler (stderr):")
+    print(result.stderr)
+
+    print("--- Python-Skript beendet ---")
+
+_run(["ls", "-l"], "/mc/server-1-21-10")
