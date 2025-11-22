c0d42c0e607a9ed720f2dd6bddfaebd5b9f00ceb
NickBelaneDev <ro.millotat@t-online.de>
2025-11-22 18:22:13 +0100

    working on the observer

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from src.config_loader import load_config
 from src.services import MinecraftServerController
 from src.server_log.state_manager import StateManager
 from src.telegram_bot.core import TelegramBot
+from src.server_log.log_watcher import stop_watching
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format="%(asctime)s - %(levelname)s - %(message)s"
@@ -14,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG,
 
 logger = logging.getLogger(__name__)
 
-# Lade Umgebungsvariablen aus der .env-Datei
+# Load environmental variables from .env
 load_dotenv(find_dotenv())
 _TOKEN = os.getenv("BOT_TOKEN")
 
@@ -28,4 +30,20 @@ if __name__ == "__main__":
     state_manager = StateManager()
 
     bot = TelegramBot(token=_TOKEN, msc=msc, state_manager=state_manager, config=config)
-    bot.run()
+
+    try:
+        bot.run()
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Shutdown signal received. Stopping services...")
+    finally:
+        # Ensure a clean shutdown of the Minecraft server and watchdog
+        if msc.is_running:
+            logger.info("Minecraft server is running, initiating shutdown.")
+            # Stop the log watcher before stopping the server
+            observer = bot.application.bot_data.get("watchdog_observer")
+            if observer:
+                stop_watching(observer)
+
+            msc.stop()
+            logger.info("Minecraft server stopped.")
+        logger.info("Application has been shut down gracefully.")
